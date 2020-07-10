a7f50946ac0b1218a9091eca519bffea6e018106
Jaba Banik <jaba.licu@gmail.com>
2020-07-10 23:47:38 +0530

    adding progress bar

--- new.py
+++ new.py
@@ -1,1 +1,18 @@
+
+import progressbar 
+import time 
+  
+  
+# Fuction to create  
+def animated_marker(): 
+      
+    widgets = ['Loading: ', progressbar.AnimatedMarker()] 
+    bar = progressbar.ProgressBar(widgets=widgets).start() 
+      
+    for i in range(50): 
+        time.sleep(0.1) 
+        bar.update(i) 
+          
+# Driver's code 
+animated_marker() 
 print("hello")
